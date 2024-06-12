24de77ba28b7521f775dedcb132a330541828c89
nikhilsbhat <nikhilsbhat93@gmail.com>
2024-06-12 17:28:52 +0530

    Initial working model for nested importing

--- pkg/yamll/yamll.go
+++ pkg/yamll/yamll.go
@@ -2,61 +2,48 @@ package yamll
 
 import (
 	"bufio"
-	"dario.cat/mergo"
 	"fmt"
-	"gopkg.in/yaml.v3"
+	"log"
 	"os"
+	"path/filepath"
 	"strings"
+
+	"dario.cat/mergo"
+	"gopkg.in/yaml.v3"
 )
 
-type YamlRoute struct {
-	Parent []string
-	Child  string
-}
-
+// YamlData holds information of yaml file and its dependency tree.
 type YamlData struct {
-	File       string
-	Data       string
-	Dependency []string
+	Root       bool                   `json:"root,omitempty" yaml:"root,omitempty"`
+	Imported   bool                   `json:"imported,omitempty" yaml:"imported,omitempty"`
+	File       string                 `json:"file,omitempty" yaml:"file,omitempty"`
+	DataRaw    string                 `json:"data_raw,omitempty" yaml:"data_raw,omitempty"`
+	Dependency []string               `json:"dependency,omitempty" yaml:"dependency,omitempty"`
+	Data       map[string]interface{} `json:"data,omitempty" yaml:"data,omitempty"`
 }
 
-func Yaml(path ...string) (string, error) {
-	routes := make([]*YamlRoute, 0)
-	for _, childPath := range path {
-		routes = append(routes, &YamlRoute{Child: childPath})
-	}
+// Config holds the information of yaml files to be parsed.
+type Config struct {
+	Files []string
+	Root  bool
+}
 
-	for _, route := range routes {
-		fileData, err := os.ReadFile(route.Child)
-		if err != nil {
-			return "", fmt.Errorf("reading child data failed with: '%v'", err)
-		}
-
-		route.Parent = dependency(string(fileData))
+// Yaml identifies the YAML imports and merges them to create a single comprehensive YAML file.
+// These imports function similarly to importing libraries in a programming language.
+func (cfg *Config) Yaml() (string, error) {
+	dependencyRoutes, err := cfg.ResolveDependencies(make(map[string]*YamlData), cfg.Files...)
+	if err != nil {
+		return "", fmt.Errorf("fetching delendency tree errored with: '%s'", err)
 	}
 
 	var importData map[string]interface{}
-	for _, route := range routes {
-		for _, parent := range route.Parent {
-			currentData, err := readYAML(parent)
-			if err != nil {
-				return "", fmt.Errorf("error reading import YAML file: '%v'", err)
-			}
-			if err = mergo.Merge(&importData, currentData, mergo.WithOverride); err != nil {
-				return "", fmt.Errorf("error merging YAML files: '%v'", err)
-			}
-		}
 
-		childData, err := readYAML(route.Child)
-		if err != nil {
-			return "", fmt.Errorf("error reading import YAML file: '%v'", err)
-		}
-		if err = mergo.Merge(&importData, childData, mergo.WithOverride); err != nil {
-			return "", fmt.Errorf("error merging YAML files: %v", err)
-		}
+	finalData, err := MergeData(importData, dependencyRoutes)
+	if err != nil {
+		return "", err
 	}
 
-	fileData, err := yaml.Marshal(importData)
+	fileData, err := yaml.Marshal(finalData)
 	if err != nil {
 		return "", fmt.Errorf("marshalling data to YAML errored with: '%v'", err)
 	}
@@ -64,37 +51,118 @@ func Yaml(path ...string) (string, error) {
 	return string(fileData), nil
 }
 
-func dependency(data string) []string {
-	dependencies := make([]string, 0)
-	stringReader := strings.NewReader(data)
+// ResolveDependencies addresses the dependencies of YAML imports specified in the YAML files.
+func (cfg *Config) ResolveDependencies(routes map[string]*YamlData, yamlFilesPath ...string) (map[string]*YamlData, error) {
+	var rootFile bool
+	if !cfg.Root {
+		rootFile = true
+	}
+	for fileHierarchy, yamlFilePath := range yamlFilesPath {
+		absYamlFilePath, err := filepath.Abs(yamlFilePath)
+		if err != nil {
+			return nil, err
+		}
 
-	scanner := bufio.NewScanner(stringReader)
-	for scanner.Scan() {
-		line := scanner.Text()
-		if strings.Contains(line, "##++") {
-			imports := strings.Split(line, ";")
-			runeSlice := []rune(imports[0])
-			dependencies = append(dependencies, string(runeSlice[4:]))
+		yamlFileData, err := os.ReadFile(absYamlFilePath)
+		if err != nil {
+			return nil, fmt.Errorf("reading YAML dependency errored with: '%v'", err)
+		}
+
+		dependencies := make([]string, 0)
+		stringReader := strings.NewReader(string(yamlFileData))
+
+		scanner := bufio.NewScanner(stringReader)
+		for scanner.Scan() {
+			line := scanner.Text()
+			if strings.Contains(line, "##++") {
+				dependencies = append(dependencies, getDependencyData(line))
+			}
+		}
+
+		if fileHierarchy == 0 && !cfg.Root {
+			cfg.Root = true
+		}
+
+		var marshalledData map[string]interface{}
+		if err = yaml.Unmarshal(yamlFileData, &marshalledData); err != nil {
+			return nil, err
+		}
+
+		routes[yamlFilePath] = &YamlData{Root: rootFile, File: yamlFilePath, DataRaw: string(yamlFileData), Data: marshalledData, Dependency: dependencies}
+
+		if len(dependencies) != 0 {
+			dependencyRoutes, err := cfg.ResolveDependencies(routes, dependencies...)
+			if err != nil {
+				return nil, err
+			}
+			if err = mergo.Merge(&routes, dependencyRoutes, mergo.WithOverride); err != nil {
+				return nil, fmt.Errorf("error merging YAML files: %v", err)
+			}
 		}
 	}
 
-	return dependencies
+	return routes, nil
 }
 
-func resolveDependencies() {
+// MergeData combines the YAML file data according to the hierarchy.
+func MergeData(src map[string]interface{}, data map[string]*YamlData) (map[string]interface{}, error) {
+	for file, fileData := range data {
+		if !fileData.Root {
+			continue
+		}
 
+		out, err := Merge(src, data, file)
+		if err != nil {
+			return nil, err
+		}
+
+		if err = mergo.Merge(&out, fileData.Data, mergo.WithOverride); err != nil {
+			return nil, fmt.Errorf("error merging YAML files: %v", err)
+		}
+		log.Printf("root file '%s' was imported successfully", file)
+
+		src = out
+	}
+
+	return src, nil
 }
 
-func readYAML(fileName string) (map[string]interface{}, error) {
-	fileData, err := os.ReadFile(fileName)
-	if err != nil {
-		return nil, err
+func Merge(src map[string]interface{}, data map[string]*YamlData, file string) (map[string]interface{}, error) {
+	for _, dependency := range data[file].Dependency {
+		if data[dependency].Imported {
+			log.Printf("file '%s' already imported hence skipping", dependency)
+			continue
+		}
+
+		out, err := Merge(src, data, dependency)
+		if err != nil {
+			return nil, err
+		}
+
+		if err := mergo.Merge(&src, out, mergo.WithOverride); err != nil {
+			return nil, fmt.Errorf("error merging YAML files: %v", err)
+		}
 	}
 
-	var data map[string]interface{}
-	if err := yaml.Unmarshal(fileData, &data); err != nil {
-		return nil, err
+	if !data[file].Imported && !data[file].Root {
+		if err := mergo.Merge(&src, data[file].Data, mergo.WithOverride); err != nil {
+			return nil, fmt.Errorf("error merging YAML files: %v", err)
+		}
+
+		data[file].Imported = true
+		log.Printf("file '%s' was imported successfully", file)
 	}
 
-	return data, nil
+	return src, nil
+}
+
+func getDependencyData(dependency string) string {
+	imports := strings.Split(dependency, ";")
+	runeSlice := []rune(imports[0])
+
+	return string(runeSlice[4:])
+}
+
+func New(path ...string) *Config {
+	return &Config{Files: path}
 }
---
+++ pkg/yamll/yamll_test.go
@@ -0,0 +1,20 @@
+package yamll_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/nikhilsbhat/yamll/pkg/yamll"
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_fetchDependency(t *testing.T) {
+	t.Run("", func(t *testing.T) {
+		cfg := yamll.New("yamll/internal/fixtures/import.yaml")
+		deps, err := cfg.Yaml()
+		assert.NoError(t, err)
+
+		fmt.Printf(deps)
+		assert.Nil(t, deps)
+	})
+}
